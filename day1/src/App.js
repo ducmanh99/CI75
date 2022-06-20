b4b955f8d46e217db3cc41890f57e1fd9b7494da
ducmanh99 <pdm241099@gmail.com>
2022-06-20 17:58:58 +0700

    file

--- day1/src/App.js
+++ day1/src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import Button from "./components/Button";
+import Title from "./components/Title";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<>
+			<div>
+				<Button type="saving" />
+				<Button type="cancel" />
+				<Button type="renting" />
+				<Button type="buy" />
+				<Button type="set auction" />
+			</div>
+			<div>
+				<Title title="hello" />
+				<Title title="chao nha" />
+				<Title title="bạn ăn cơm chưa" />
+				<Title title="hey do" />
+				<Title title="what sub" />
+			</div>
+		</>
+	);
 }
 
 export default App;
---
+++ day1/src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({type}) => {
+  return (
+    <button>{type}</button>
+  )
+}
+
+export default Button
